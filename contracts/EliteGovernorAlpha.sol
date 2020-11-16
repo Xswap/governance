fd16d413c7a720dbb33674ca63e278a3141b46b3
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2020-11-16 18:20:54 +0200

    Change quorumVotes and proposalThreshold in EliteGovernorAlpha contract

--- contracts/EliteGovernorAlpha.sol
+++ contracts/EliteGovernorAlpha.sol
@@ -6,10 +6,10 @@ contract EliteGovernorAlpha {
     string public constant name = "Eliteswap Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 40_000_000e18; } // 4% of Elt
+    function quorumVotes() public pure returns (uint) { return 400_000_000e18; } // 4% of Elt
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 10_000_000e18; } // 1% of Elt
+    function proposalThreshold() public pure returns (uint) { return 100_000_000e18; } // 1% of Elt
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
