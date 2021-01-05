260a22f57e2d2c1f8210619cb75054e17cd8e1a2
Cryptocode <crytocode99@gmail.com>
2021-01-06 01:09:55 +0700

    Update NikiToSetter.sol

--- contracts/NikiToSetter.sol
+++ contracts/NikiToSetter.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.16;
 
 // this contract serves as feeToSetter, allowing owner to manage fees in the context of a specific feeTo implementation
-contract XswapFeeToSetter {
+contract NikiFeeToSetter {
     // immutables
     address public factory;
     uint public vestingEnd;
@@ -10,7 +10,7 @@ contract XswapFeeToSetter {
     address public owner;
 
     constructor(address factory_, uint vestingEnd_, address owner_, address feeTo_) public {
-        require(vestingEnd_ > block.timestamp, 'XswapFeeToSetter::constructor: vesting must end after deployment');
+        require(vestingEnd_ > block.timestamp, 'NikiFeeToSetter::constructor: vesting must end after deployment');
         factory = factory_;
         vestingEnd = vestingEnd_;
         owner = owner_;
@@ -19,26 +19,26 @@ contract XswapFeeToSetter {
 
     // allows owner to change itself at any time
     function setOwner(address owner_) public {
-        require(msg.sender == owner, 'XswapFeeToSetter::setOwner: not allowed');
+        require(msg.sender == owner, 'NikiFeeToSetter::setOwner: not allowed');
         owner = owner_;
     }
 
     // allows owner to change feeToSetter after vesting
     function setFeeToSetter(address feeToSetter_) public {
-        require(block.timestamp >= vestingEnd, 'XswapFeeToSetter::setFeeToSetter: not time yet');
-        require(msg.sender == owner, 'XswapFeeToSetter::setFeeToSetter: not allowed');
-        IXswapV2Factory(factory).setFeeToSetter(feeToSetter_);
+        require(block.timestamp >= vestingEnd, 'NikiFeeToSetter::setFeeToSetter: not time yet');
+        require(msg.sender == owner, 'NikiFeeToSetter::setFeeToSetter: not allowed');
+        INikiswapV2Factory(factory).setFeeToSetter(feeToSetter_);
     }
 
     // allows owner to turn fees on/off after vesting
     function toggleFees(bool on) public {
-        require(block.timestamp >= vestingEnd, 'XswapFeeToSetter::toggleFees: not time yet');
-        require(msg.sender == owner, 'XswapFeeToSetter::toggleFees: not allowed');
-        IXswapV2Factory(factory).setFeeTo(on ? feeTo : address(0));
+        require(block.timestamp >= vestingEnd, 'NikiFeeToSetter::toggleFees: not time yet');
+        require(msg.sender == owner, 'NikiFeeToSetter::toggleFees: not allowed');
+        INikiswapV2Factory(factory).setFeeTo(on ? feeTo : address(0));
     }
 }
 
-interface IXswapV2Factory {
+interface INikiswapV2Factory {
     function setFeeToSetter(address) external;
     function setFeeTo(address) external;
 }
